d15e20b640d162d5b6d1cba33b80bd240177125f
Dan Okhlopkov <danokhlopkov@gmail.com>
2019-06-23 14:54:55 +0300

    add publish endpoint

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify
-from flask import request
+import os
+import requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 import generate_gifs
+
+CHANNEL_CHAT_ID = -1001338525741
 
 template_numbers = generate_gifs.get_template_numbers()
 
@@ -11,9 +14,11 @@ template_numbers = generate_gifs.get_template_numbers()
 def index():
     return "Hello, World!"
 
+
 @app.route('/templates', methods=['GET'])
 def get_template():
     return jsonify({'templates': template_numbers})
+
 
 @app.route('/filter', methods=['GET'])
 def filter_image():
@@ -25,5 +30,26 @@ def filter_image():
     return jsonify({'url': result_url})
 
 
+@app.route('/publish', methods=['GET'])
+def publist_to_telegram():
+    TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+    content_url = request.args.get('url')
+
+    r = requests.get(
+        "https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}".format(
+            token=TELEGRAM_TOKEN,
+            chat_id=CHANNEL_CHAT_ID,
+            text=content_url
+        )
+    )
+
+    return jsonify({'status': r.ok, 'desc': r.text})
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, threaded=True, port=80)
+    app.run(
+        host="0.0.0.0", 
+        debug=True, 
+        threaded=True, 
+        port=80
+    )
